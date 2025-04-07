85d258657330dd0941da58a9313294ec0344262f
samkaran070 <freemovieslogin@gmail.com>
2025-04-07 17:55:20 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
+api_id = 24244751
+api_hash = "3f736b7021e43679b5269946b4358c6d"
+bot_token = ""
 auth_users = [1713924419]
 sudo_user = 1713924419
 log_channel = -1002143629860
